e451113356748780784013daea8ddcf9d04e409e
rawfiner <rawfiner@gmail.com>
2020-12-24 16:38:07 +0100

    cacorrectrgb: attempt at blending several radius

--- src/iop/cacorrectrgb.c
+++ src/iop/cacorrectrgb.c
@@ -89,10 +89,12 @@ void commit_params(dt_iop_module_t *self, dt_iop_params_t *p1, dt_dev_pixelpipe_
 }
 
 static void ca_correct_rgb(const float* const restrict in, const size_t width, const size_t height,
-                          const size_t ch, const size_t iterations,
+                          const size_t ch, const float sigma,
                           const dt_iop_cacorrectrgb_guide_channel_t guide,
                           float* const restrict out)
 {
+  //TODO do all computation with downscaled image
+
   float *const restrict blurred_in = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
   float *const restrict manifold_higher = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
   float *const restrict manifold_lower = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
@@ -125,7 +127,6 @@ dt_omp_firstprivate(in, width, height) \
     if(pixelb > maxb) maxb = pixelb;
   }
 
-  const float sigma = iterations;
   float max[4] = {maxr, maxg, maxb, 1.0f};
   float min[4] = {fminf(minr, 0.0f), fminf(ming, 0.0f), fminf(minb, 0.0f), 0.0f};
   dt_gaussian_t *g = dt_gaussian_init(width, height, 4, max, min, sigma, 0);
@@ -177,9 +178,12 @@ dt_omp_firstprivate(blurred_manifold_lower, blurred_manifold_higher, width, heig
     }
   }
 
+  //TODO upscale blurred_manifolds and blurred_in here
+  // for this to be worth it we need upscaling to be faster than gaussian blur
+
 #ifdef _OPENMP
 #pragma omp parallel for default(none) \
-dt_omp_firstprivate(in, width, height, iterations, guide, blurred_in, blurred_manifold_higher, blurred_manifold_lower, out) \
+dt_omp_firstprivate(in, width, height, guide, blurred_in, blurred_manifold_higher, blurred_manifold_lower, out) \
   schedule(static)
 #endif
   for(size_t i = 0; i < height; i++)
@@ -215,6 +219,7 @@ dt_omp_firstprivate(in, width, height, iterations, guide, blurred_in, blurred_ma
         float ratio = dist * ratio_means + (1.0f - dist) * ratio_means_manifold;
         out[(i * width + j) * 4 + c] = in[(i * width + j) * 4 + guide] * ratio;
       }
+      out[(i * width + j) * 4 + guide] = in[(i * width + j) * 4 + guide];
     }
   }
   dt_free_align(blurred_in);
@@ -233,15 +238,127 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const 
   const float* in = (float*)ivoid;
   float* out = (float*)ovoid;
 
-  memcpy(out, in, width * height * ch * sizeof(float));
   if(ch != 4 || (piece->pipe->type & DT_DEV_PIXELPIPE_PREVIEW) == DT_DEV_PIXELPIPE_PREVIEW
      || (piece->pipe->type & DT_DEV_PIXELPIPE_THUMBNAIL) == DT_DEV_PIXELPIPE_THUMBNAIL)
   {
+    memcpy(out, in, width * height * ch * sizeof(float));
     return;
   }
+
+  float *const restrict out0_s = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+  float *const restrict out0_4s = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+  float *const restrict out0_16s = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+  float *const restrict out1_s = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+  float *const restrict out1_4s = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+  float *const restrict out1_16s = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+  float *const restrict out2_s = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+  float *const restrict out2_4s = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+  float *const restrict out2_16s = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+  float *const restrict diffs_1 = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+  float *const restrict diffs_2 = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+  float *const restrict blurred_diffs_1 = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+  float *const restrict blurred_diffs_2 = dt_alloc_sse_ps(dt_round_size_sse(width * height * ch));
+
   const dt_iop_cacorrectrgb_guide_channel_t guide = d->guide_channel;
-  const size_t rad = MAX(d->radius / scale, 1);
-  ca_correct_rgb(in, width, height, ch, rad, guide, out);
+  const float sigma = MAX(d->radius / scale, 1);
+  // we compute the correction 3 times and them blend them
+  // in order to have an adaptative correction depending on
+  // the amount of chromatic aberration in each part of the
+  // image
+  ca_correct_rgb(in, width, height, ch, sigma, guide, out0_s);
+  ca_correct_rgb(in, width, height, ch, 4.0f * sigma, guide, out0_4s);
+  ca_correct_rgb(in, width, height, ch, 16.0f * sigma, guide, out0_16s);
+  ca_correct_rgb(out0_s, width, height, ch, sigma, (guide + 1) % 3, out1_s);
+  ca_correct_rgb(out0_4s, width, height, ch, 4.0f * sigma, (guide + 1) % 3, out1_4s);
+  ca_correct_rgb(out0_16s, width, height, ch, 16.0f * sigma, (guide + 1) % 3, out1_16s);
+  ca_correct_rgb(out0_s, width, height, ch, sigma, (guide + 2) % 3, out2_s);
+  ca_correct_rgb(out0_4s, width, height, ch, 4.0f * sigma, (guide + 2) % 3, out2_4s);
+  ca_correct_rgb(out0_16s, width, height, ch, 16.0f * sigma, (guide + 2) % 3, out2_16s);
+#ifdef _OPENMP
+#pragma omp parallel for simd default(none) \
+dt_omp_firstprivate(in, out0_s, out0_4s, out0_16s, out1_s, out1_4s, out1_16s, out2_s, out2_4s, out2_16s, diffs_1, diffs_2, width, height, guide) \
+  schedule(simd:static) aligned(in, out0_s, out0_4s, out0_16s, out1_s, out1_4s, out1_16s, out2_s, out2_4s, out2_16s, diffs_1, diffs_2:64)
+#endif
+  for(size_t k = 0; k < width * height; k++)
+  {
+    const float ref_pixelg = fmaxf(in[k * 4 + guide], 1E-6);
+    // out[k * 4 + guide] = ref_pixelg;
+    float diff1_1 = fmaxf(fabsf(out1_s[k * 4 + guide] - ref_pixelg), 1E-6) / ref_pixelg;
+    float diff1_4 = fmaxf(fabsf(out1_4s[k * 4 + guide] - ref_pixelg), 1E-6) / ref_pixelg;
+    float diff1_16 = fmaxf(fabsf(out1_16s[k * 4 + guide] - ref_pixelg), 1E-6) / ref_pixelg;
+    float w11 = fminf(diff1_1 * diff1_1, 4.0f);
+    float w44 = fminf(diff1_4 * diff1_4, 4.0f);
+    float w1616 = fminf(diff1_16 * diff1_16, 4.0f);
+    diffs_1[k * 4] = w11;
+    diffs_1[k * 4 + 1] = w44;
+    diffs_1[k * 4 + 2] = w1616;
+    // size_t c = (guide + 1) % 3;
+    // out[k * 4 + c] = (in[k * 4 + c] * w00 + out0_s[k * 4 + c] * w11 + out0_4s[k * 4 + c] * w44 + out0_16s[k * 4 + c] * w1616) / (w00 + w11 + w44 + w1616);
+    float diff2_1 = fmaxf(fabsf(out2_s[k * 4 + guide] - ref_pixelg), 1E-6) / ref_pixelg;
+    float diff2_4 = fmaxf(fabsf(out2_4s[k * 4 + guide] - ref_pixelg), 1E-6) / ref_pixelg;
+    float diff2_16 = fmaxf(fabsf(out2_16s[k * 4 + guide] - ref_pixelg), 1E-6) / ref_pixelg;
+    w11 = fminf(diff2_1 * diff2_1, 4.0f);
+    w44 = fminf(diff2_4 * diff2_4, 4.0f);
+    w1616 = fminf(diff2_16 * diff2_16, 4.0f);
+    diffs_2[k * 4] = w11;
+    diffs_2[k * 4 + 1] = w44;
+    diffs_2[k * 4 + 2] = w1616;
+    // c = (guide + 2) % 3;
+    // out[k * 4 + c] = (in[k * 4 + c] * w00 + out0_s[k * 4 + c] * w11 + out0_4s[k * 4 + c] * w44 + out0_16s[k * 4 + c] * w1616) / (w00 + w11 + w44 + w1616);
+    // out[k * 4 + 3] = in[k * 4 + 3];
+  }
+  float max[4] = {4.0f, 4.0f, 4.0f, 0.0f};
+  float min[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+  dt_gaussian_t *g = dt_gaussian_init(width, height, 4, max, min, sigma * 16.0f, 0);
+  if(!g) return;
+  dt_gaussian_blur_4c(g, diffs_1, blurred_diffs_1);
+  dt_gaussian_blur_4c(g, diffs_2, blurred_diffs_2);
+
+#ifdef _OPENMP
+#pragma omp parallel for simd default(none) \
+dt_omp_firstprivate(in, out0_s, out0_4s, out0_16s, out1_s, out1_4s, out1_16s, out2_s, out2_4s, out2_16s, blurred_diffs_1, blurred_diffs_2, out, width, height, guide) \
+  schedule(simd:static) aligned(in, out0_s, out0_4s, out0_16s, out1_s, out1_4s, out1_16s, out2_s, out2_4s, out2_16s, blurred_diffs_1, blurred_diffs_2, out:64)
+#endif
+  for(size_t k = 0; k < width * height; k++)
+  {
+    const float ref_pixelg = fmaxf(in[k * 4 + guide], 1E-6);
+    out[k * 4 + guide] = ref_pixelg;
+    float w11 = 1.0f / fmaxf(blurred_diffs_1[k * 4], 1E-6);
+    float w44 = 1.0f / fmaxf(blurred_diffs_1[k * 4 + 1], 1E-6);
+    float w1616 = 1.0f / fmaxf(blurred_diffs_1[k * 4 + 2], 1E-6);
+    float w00 = w11;
+    w00 *= w00;
+    w11 *= w11;
+    w44 *= w44;
+    w1616 *= w1616;
+    size_t c = (guide + 1) % 3;
+    out[k * 4 + c] = (in[k * 4 + c] * w00 + out0_s[k * 4 + c] * w11 + out0_4s[k * 4 + c] * w44 + out0_16s[k * 4 + c] * w1616) / (w00 + w11 + w44 + w1616);
+    w11 = 1.0f / fmaxf(blurred_diffs_2[k * 4], 1E-6);
+    w44 = 1.0f / fmaxf(blurred_diffs_2[k * 4 + 1], 1E-6);
+    w1616 = 1.0f / fmaxf(blurred_diffs_2[k * 4 + 2], 1E-6);
+    w00 = w11;
+    w00 *= w00;
+    w11 *= w11;
+    w44 *= w44;
+    w1616 *= w1616;
+    c = (guide + 2) % 3;
+    out[k * 4 + c] = (in[k * 4 + c] * w00 + out0_s[k * 4 + c] * w11 + out0_4s[k * 4 + c] * w44 + out0_16s[k * 4 + c] * w1616) / (w00 + w11 + w44 + w1616);
+    out[k * 4 + 3] = in[k * 4 + 3];
+  }
+
+  dt_free_align(out0_s);
+  dt_free_align(out0_4s);
+  dt_free_align(out0_16s);
+  dt_free_align(out1_s);
+  dt_free_align(out1_4s);
+  dt_free_align(out1_16s);
+  dt_free_align(out2_s);
+  dt_free_align(out2_4s);
+  dt_free_align(out2_16s);
+  dt_free_align(diffs_1);
+  dt_free_align(diffs_2);
+  dt_free_align(blurred_diffs_1);
+  dt_free_align(blurred_diffs_2);
 }
 
 /** gui setup and update, these are needed. */
