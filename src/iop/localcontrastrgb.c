a92867c846a398251c66e4af8d1d198a2f1e9351
rawfiner <rawfiner@gmail.com>
2021-05-11 20:24:41 +0200

    localcontrastrgb: full algo. but does not work that well...
    eigf is not the best filter for local contrast
    in areas where eigf keeps input, local contrast is not
    increased, and we can see it

--- src/iop/localcontrastrgb.c
+++ src/iop/localcontrastrgb.c
@@ -36,7 +36,7 @@ DT_MODULE_INTROSPECTION(1, dt_iop_localcontrastrgb_params_t)
 typedef struct dt_iop_localcontrastrgb_params_t
 {
   dt_iop_rgb_norms_t norm; // $DEFAULT: DT_RGB_NORM_LUMINANCE $DESCRIPTION: "rgb norm"
-  float blending; // $MIN: 0.01 $MAX: 100.0 $DEFAULT: 5.0 $DESCRIPTION: "smoothing diameter"
+  float blending; // $MIN: 0.01 $MAX: 1000.0 $DEFAULT: 5.0 $DESCRIPTION: "smoothing diameter"
   float feathering; // $MIN: 0.01 $MAX: 10000.0 $DEFAULT: 1.0 $DESCRIPTION: "edges refinement/feathering"
   float strength; // $MIN: 0 $MAX: 400 $DEFAULT: 100 $DESCRIPTION: "strength"
 } dt_iop_localcontrastrgb_params_t;
@@ -83,12 +83,70 @@ void commit_params(dt_iop_module_t *self, dt_iop_params_t *p1, dt_dev_pixelpipe_
 void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const void *const ivoid, void *const ovoid,
              const dt_iop_roi_t *const roi_in, const dt_iop_roi_t *const roi_out)
 {
+  dt_iop_localcontrastrgb_params_t *d = (dt_iop_localcontrastrgb_params_t *)self->params;
   const int ch = piece->colors;
   const size_t width = roi_out->width;
   const size_t height = roi_out->height;
   const float* in = (float*)ivoid;
   float* out = (float*)ovoid;
-  memcpy(out, in, width * height * ch * sizeof(float));
+  const dt_iop_rgb_norms_t norm = d->norm;
+  const float strength = d->strength / 100.0f;
+  const dt_iop_order_iccprofile_info_t *const work_profile = dt_ioppr_get_pipe_work_profile_info(piece->pipe);
+  const int max_size = (piece->iwidth > piece->iheight) ? piece->iwidth : piece->iheight;
+  const float scale = piece->iscale / roi_in->scale;
+  const float diameter = d->blending / scale + 0.0f * max_size * roi_in->scale;
+  const float radius = (diameter - 1.0f) / 2.0f;
+  const float feathering = 1.0f / d->feathering;
+
+  float *const restrict ratios = dt_alloc_align_float(width * height * ch);
+  float *const restrict norm_in = dt_alloc_align_float(width * height);
+  float *const restrict norm_blurred = dt_alloc_align_float(width * height);
+
+  // compute rgb norm, and rgb ratios
+#ifdef _OPENMP
+#pragma omp parallel for simd default(none) \
+dt_omp_firstprivate(in, norm_in, norm_blurred, ratios, norm, work_profile, width, height, ch) \
+  schedule(simd:static) aligned(in, norm_in, norm_blurred, ratios:64)
+#endif
+  for(size_t k = 0; k < width * height; k++)
+  {
+    const float n = fmaxf(dt_rgb_norm(in + 4 * k, norm, work_profile), 1E-6);
+    norm_in[k] = n;
+    norm_blurred[k] = n;
+    //TODO: use forall loop
+    for(size_t c = 0; c < 3; c++)
+    {
+      ratios[k * 4 + c] = in[k * 4 + c] / n;
+    }
+  }
+
+  // eigf
+  fast_eigf_surface_blur(norm_blurred, width, height, radius, feathering, 1,
+                 DT_GF_BLENDING_LINEAR, 1.0f, 0.0f, exp2f(-14.0f), 4.0f);
+
+  // compute (log?) difference between norm and eigf
+  // amplify difference by strength
+  // add it back to eigf result
+  // compute rgb image from this and rgb ratios
+#ifdef _OPENMP
+#pragma omp parallel for simd default(none) \
+dt_omp_firstprivate(in, norm_in, norm_blurred, ratios, out, width, height, ch, strength) \
+  schedule(simd:static) aligned(in, norm_in, norm_blurred, out, ratios:64)
+#endif
+  for(size_t k = 0; k < width * height; k++)
+  {
+    const float result_norm = exp2f(log2f(norm_in[k] / norm_blurred[k]) * strength) * norm_blurred[k];
+    //const float result_norm = (norm_in[k] - norm_blurred[k]) * strength + norm_blurred[k];
+    //TODO: use forall loop
+    for(size_t c = 0; c < 3; c++)
+    {
+      out[k * 4 + c] = ratios[k * 4 + c] * result_norm;
+    }
+    out[k * 4 + 3] = in[k * 4 + 3];
+  }
+
+
+  //memcpy(out, in, width * height * ch * sizeof(float));
 }
 
 void gui_update(dt_iop_module_t *self)
