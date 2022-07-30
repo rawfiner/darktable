deba67201bbf9a3961c092e3ae0b69c68a0d9b9c
rawfiner <rawfiner@gmail.com>
2022-07-30 17:26:05 +0200

    denoiseprofiled: symrbf: add last symmetry axis

--- src/iop/denoiseprofile.c
+++ src/iop/denoiseprofile.c
@@ -1354,6 +1354,23 @@ static void process_symrbf(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t 
       avg_diff /= ((2.0f * radius + 1.0f) * radius);
       symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_HORIZ_AXIS] = avg_diff;
 
+      // looking for symmetry along the top-right -> bottom-left axis
+      avg_diff = 0.0f;
+      // iterate on top right corner
+      for(int64_t ii = -radius; ii <= radius; ii++)
+      {
+        for(int64_t jj = ii+1; jj <= radius; jj++)
+        {
+          // coordinates of symmetry point
+          int64_t symi = -jj;
+          int64_t symj = -ii;
+          float diff = precond[(width * (i + ii) + j + jj) * 4 + 0] - precond[(width * (i + symi) + j +  symj) * 4 + 0];
+          avg_diff += diff * diff;
+        }
+      }
+      avg_diff /= ((2.0f * radius + 1.0f) * radius);
+      symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_TOPRIGHT_BOTLEFT_AXIS] = avg_diff;
+
     }
   }
   // first pass:
@@ -1389,13 +1406,15 @@ static void process_symrbf(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t 
       const float weighth = 100.0f * d->strength * expf(-symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_VERT_AXIS] / (50.0f * d->nbhood));
       const float weightv = 100.0f * d->strength * expf(-symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_HORIZ_AXIS] / (50.0f * d->nbhood));
       const float weighttrbl = 100.0f * d->strength * expf(-symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_TOPLEFT_BOTRIGHT_AXIS] / (50.0f * d->nbhood));
+      const float weighttlbr = 100.0f * d->strength * expf(-symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_TOPRIGHT_BOTLEFT_AXIS] / (50.0f * d->nbhood));
       const float weightc[4] = {10.0f, 1.0f, 1.0f, 1.0f}; // smooth less Y0
       for(size_t c = 0; c < 4; c++)
       {
         out[((width * i) + j) * 4 + c] = (weightc[c] * precond[((width * i) + j) * 4 + c]
                                         + weighth * out[((width * i) + j-1) * 4 + c]
                                         + weightv * out[((width * (i-1)) + j) * 4 + c]
-                                        + weighttrbl * out[((width * (i-1)) + j+1) * 4 + c]) / (weightc[c] + weighth + weightv + weighttrbl);
+                                        + weighttlbr * out[((width * (i-1)) + j-1) * 4 + c]
+                                        + weighttrbl * out[((width * (i-1)) + j+1) * 4 + c]) / (weightc[c] + weighth + weightv + weighttlbr + weighttrbl);
       }
     }
   }
