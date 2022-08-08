7897fa80fead6d205f3235cdcb5cc1a18710d2cf
rawfiner <rawfiner@gmail.com>
2022-08-08 21:20:56 +0200

    denoiseprofile: code refactoring

--- src/iop/denoiseprofile.c
+++ src/iop/denoiseprofile.c
@@ -1341,6 +1341,86 @@ static void compute_symmetry(const float* const restrict in, float* restrict sym
   }
 }
 
+static void rbf_topleft_bottomright(float* restrict out, const float* const restrict in, const float* const restrict symfactors, const size_t height, const size_t width, const int64_t radius, const float strength, const float weightY0)
+{
+  memcpy(out, in, width * height * 4 * sizeof(float));
+  // pass from top to bottom and left to right.
+  // we diffuse using left, top left, top, and top right pixels
+  //TODO copy first radius+1 lines
+
+  for(int64_t i = radius+1; i < height-radius-1; i++)
+  {
+    // for all other lines, diffuse
+
+    //TODO copy first radius+1 column
+
+    for(int64_t j = radius+1; j < width-radius-1; j++)
+    {
+      // maybe: adapt the strength factor in front of weightv depending
+      // on the quantity of pixels already involved in the average? Or to the
+      // maximum weight of a pixel currently in the average?
+      float weighth = 10000.0f * strength / fmaxf(symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_VERT_AXIS], 0.001f);
+      float weightv = 10000.0f * strength / fmaxf(symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_HORIZ_AXIS], 0.001f);
+      float weighttrbl = 10000.0f * strength / fmaxf(symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_TOPLEFT_BOTRIGHT_AXIS], 0.001f);
+      float weighttlbr = 10000.0f * strength / fmaxf(symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_TOPRIGHT_BOTLEFT_AXIS], 0.001f);
+      const float weightc[4] = {10.0f * weightY0, 1.0f, 1.0f, 1.0f}; // smooth less Y0
+      // keep only the 2 best directions
+      // float* weights[4];
+      // weights[0] = &weighth;
+      // weights[1] = &weightv;
+      // weights[2] = &weighttrbl;
+      // weights[3] = &weighttlbr;
+      // float min = weighth;
+      // float* min_address = &weighth;
+      // for(int k = 1; k < 4; k++)
+      // {
+      //   if(weights[k][0] < min)
+      //   {
+      //     min = weights[k][0];
+      //     min_address = weights[k];
+      //   }
+      // }
+      // *min_address = 0.0f;
+      // min = 10000000000000.0f;
+      // min_address = NULL;
+      // for(int k = 0; k < 4; k++)
+      // {
+      //   if(weights[k][0] == 0.0f) continue;
+      //   if(weights[k][0] < min)
+      //   {
+      //     min = weights[k][0];
+      //     min_address = weights[k];
+      //   }
+      // }
+      // if(min_address != NULL)
+      //   *min_address = 0.0f;
+
+      weighth = fminf(weighth, (j-radius) * 0.2f);
+      weightv = fminf(weightv, (i-radius) * 0.2f);
+      weighttlbr = fminf(weighttlbr, (j-radius) * 0.2f);
+      weighttlbr = fminf(weighttlbr, (i-radius) * 0.2f);
+      weighttrbl = fminf(weighttrbl, (j-radius) * 0.2f);
+      weighttrbl = fminf(weighttrbl, (i-radius) * 0.2f);
+
+      for(size_t c = 0; c < 3; c++)
+      {
+        out[((width * i) + j) * 4 + c] = (weightc[c] * in[((width * i) + j) * 4 + c]
+                                        + weighth * out[((width * i) + j-1) * 4 + c]
+                                        + weightv * out[((width * (i-1)) + j) * 4 + c]
+                                        + weighttlbr * out[((width * (i-1)) + j-1) * 4 + c]
+                                        + weighttrbl * out[((width * (i-1)) + j+1) * 4 + c]) / (weightc[c] + weighth + weightv + weighttlbr + weighttrbl);
+      }
+      //TODO: store in c=4 the maximum weight a pixel of this average has. Then, make sure the weight of any previous average is not higher than 1/max.
+      //for instance, if we had one pixel in the blur, the max weight will be 1.
+      // if we had 2 pixels in the blur, the max weight will be 1/2, so the max
+      // weighth will be 2.
+      // this makes sure that border pixels don't get an overwhelming importance
+    }
+    //TODO copy last radius+1 columns
+  }
+}
+
+
 static void process_symrbf(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece,
                              const void *const ivoid, void *const ovoid, const dt_iop_roi_t *const roi_in,
                              const dt_iop_roi_t *const roi_out)
@@ -1378,87 +1458,12 @@ static void process_symrbf(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t 
   const int64_t radius = d->radius;
   precondition_Y0U0V0(in, precond, width, height, d->a[1] * compensate_p, p, d->b[1], toY0U0V0);
 
-  compute_symmetry(precond, symfactors, height, width, radius);
+  compute_symmetry(precond, symfactors, height, width, radius * 2);
+  rbf_topleft_bottomright(out, precond, symfactors, height, width, radius * 2, d->strength, d->central_pixel_weight);
 
-  memcpy(out, precond, width * height * 4 * sizeof(float));
-  // first pass:
-  // from top to bottom and left to right.
-  // we diffuse using left, top left, top, and top right pixels
-  //TODO copy first radius+1 lines
-
-  for(int64_t i = radius+1; i < height-radius-1; i++)//FIXME: start at radius, not 0
-  {
-    // for all other lines, diffuse
-
-    //TODO copy first radius+1 column
-
-    for(int64_t j = radius+1; j < width-radius-1; j++)
-    {
-      // maybe: adapt the strength factor in front of weightv depending
-      // on the quantity of pixels already involved in the average? Or to the
-      // maximum weight of a pixel currently in the average?
-      float weighth = 10000.0f * d->strength / fmaxf(symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_VERT_AXIS], 0.001f);
-      float weightv = 10000.0f * d->strength / fmaxf(symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_HORIZ_AXIS], 0.001f);
-      float weighttrbl = 10000.0f * d->strength / fmaxf(symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_TOPLEFT_BOTRIGHT_AXIS], 0.001f);
-      float weighttlbr = 10000.0f * d->strength / fmaxf(symfactors[((width * i) + j) * 4 + DT_DENOISE_PROFILE_SYM_TOPRIGHT_BOTLEFT_AXIS], 0.001f);
-      const float weightc[4] = {10.0f * d->central_pixel_weight, 1.0f, 1.0f, 1.0f}; // smooth less Y0
-      // keep only the 2 best directions
-      float* weights[4];
-      weights[0] = &weighth;
-      weights[1] = &weightv;
-      weights[2] = &weighttrbl;
-      weights[3] = &weighttlbr;
-      float min = weighth;
-      float* min_address = &weighth;
-      for(int k = 1; k < 4; k++)
-      {
-        if(weights[k][0] < min)
-        {
-          min = weights[k][0];
-          min_address = weights[k];
-        }
-      }
-      *min_address = 0.0f;
-      min = 10000000000000.0f;
-      min_address = NULL;
-      for(int k = 0; k < 4; k++)
-      {
-        if(weights[k][0] == 0.0f) continue;
-        if(weights[k][0] < min)
-        {
-          min = weights[k][0];
-          min_address = weights[k];
-        }
-      }
-      if(min_address != NULL)
-        *min_address = 0.0f;
-
-      weighth = fminf(weighth, (j-radius) * 0.2f);
-      weightv = fminf(weightv, (i-radius) * 0.2f);
-      weighttlbr = fminf(weighttlbr, (j-radius) * 0.2f);
-      weighttlbr = fminf(weighttlbr, (i-radius) * 0.2f);
-      weighttrbl = fminf(weighttrbl, (j-radius) * 0.2f);
-      weighttrbl = fminf(weighttrbl, (i-radius) * 0.2f);
-
-      for(size_t c = 0; c < 3; c++)
-      {
-        out[((width * i) + j) * 4 + c] = (weightc[c] * precond[((width * i) + j) * 4 + c]
-                                        + weighth * out[((width * i) + j-1) * 4 + c]
-                                        + weightv * out[((width * (i-1)) + j) * 4 + c]
-                                        + weighttlbr * out[((width * (i-1)) + j-1) * 4 + c]
-                                        + weighttrbl * out[((width * (i-1)) + j+1) * 4 + c]) / (weightc[c] + weighth + weightv + weighttlbr + weighttrbl);
-      }
-      //TODO: store in c=4 the maximum weight a pixel of this average has. Then, make sure the weight of any previous average is not higher than 1/max.
-      //for instance, if we had one pixel in the blur, the max weight will be 1.
-      // if we had 2 pixels in the blur, the max weight will be 1/2, so the max
-      // weighth will be 2.
-      // this makes sure that border pixels don't get an overwhelming importance
-    }
-
-    //TODO copy last radius+1 columns
-
-  }
   //TODO copy last radius+1 lines
+  compute_symmetry(out, symfactors, height, width, radius);
+  rbf_topleft_bottomright(out, precond, symfactors, height, width, radius, d->strength, d->central_pixel_weight);
 
   //memcpy(out, precond, width * height * piece->colors * sizeof(float));
   backtransform_Y0U0V0(out, width, height, d->a[1] * compensate_p, p, d->b[1], d->bias - 0.5 * logf(in_scale), wb, toRGB);
